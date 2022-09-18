c1f6dd14e99aa1973c5c85cfdf37c099c7ae87c5
cjclap <dmxtechinn@gmail.com>
2022-09-17 19:04:30 -0500

    Rewrite of the code

---
+++ assets/script2.js
@@ -0,0 +1,5 @@
+var d = new Date();
+let date = d.getDay();
+document.getElementById("date").innerHTML = d;
+let begining = document.getElementById("begining");
+let quizQuestions = document.getElementById("quiz");
